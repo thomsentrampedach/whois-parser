2fe468ae5dcb50d478d19bfa8d3b6683503fcdfc
John Thomas <thomas07@vt.edu>
2018-02-13 09:11:20 -0500

    adding corehub parser

---
+++ lib/whois/parsers/whois.corehub.net.rb
@@ -0,0 +1,15 @@
+require_relative 'base_icann_compliant'
+
+module Whois
+  class Parsers
+
+    # Parser for the whois.corehub.net server.
+    #
+    # @see Whois::Parsers::Example
+    #   The Example parser for the list of all available methods.
+    #
+    class WhoisCorehubNet < BaseIcannCompliant
+    end
+
+  end
+end
