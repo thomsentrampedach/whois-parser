678d0349add66e51b8026d7d0f9159103222f2fa
John Thomas <thomas07@vt.edu>
2018-02-15 15:21:16 -0500

    adding parser for whois.launchpad.com

---
+++ lib/whois/parsers/whois.launchpad.com.rb
@@ -0,0 +1,16 @@
+require_relative 'base_icann_compliant'
+
+module Whois
+  class Parsers
+
+    # Parser for the whois.biz server.
+    #
+    # @see Whois::Parsers::Example
+    #   The Example parser for the list of all available methods.
+    #
+    class WhoisLaunchpadCom < BaseIcannCompliant
+    end
+  end
+end
+
+
