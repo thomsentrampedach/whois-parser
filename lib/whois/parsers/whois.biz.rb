cca85b74881a0a8e5985199275e1bc4c7294ec15
John Thomas <thomas07@vt.edu>
2018-02-15 10:05:55 -0500

    Revert "whois.biz server moved to icant n format"
    This reverts commit 45f1f5bde6dd417eaed28d75d1adff97ea10add4.

--- lib/whois/parsers/whois.biz.rb
+++ lib/whois/parsers/whois.biz.rb
@@ -7,7 +7,7 @@
 #++
 
 
-require_relative 'base_icann_compliant'
+require_relative 'base_shared2'
 
 
 module Whois
@@ -18,16 +18,7 @@ module Whois
     # @see Whois::Parsers::Example
     #   The Example parser for the list of all available methods.
     #
-    class WhoisBiz < BaseIcannCompliant
-      self.scanner = Scanners::BaseIcannCompliant, {
-          pattern_available: /^No Data Found\n/
-      }
-
-      property_supported :expires_on do
-        node("Registry Expiry Date") do |value|
-          parse_time(value)
-        end
-      end
+    class WhoisBiz < BaseShared2
     end
 
   end
