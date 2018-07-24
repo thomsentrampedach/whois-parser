6f9de173155bfcc8bf73474a74fc063a052dc6d4
John Thomas <thomas07@vt.edu>
2018-07-24 14:24:09 -0400

    updating whois.amnic.net

--- lib/whois/parsers/whois.amnic.net.rb
+++ lib/whois/parsers/whois.amnic.net.rb
@@ -73,7 +73,10 @@ module Whois
         end
       end
 
+      property_not_supported :registrar
+      property_not_supported :technical_contacts
+      property_not_supported :admin_contacts
+      property_not_supported :registrant_contacts
     end
-
   end
 end
