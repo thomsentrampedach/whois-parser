15c8e2a5d30d9a6b9e5de091859f3e4fed9a07fb
John Thomas <thomas07@vt.edu>
2018-07-25 12:26:30 -0400

    adding a bunch of new parsers

---
+++ lib/whois/parsers/whois.nic.team.rb
@@ -0,0 +1,12 @@
+require_relative 'base_icann_compliant'
+module Whois
+  class Parsers
+    class WhoisNicTeam < BaseIcannCompliant
+      self.scanner = Scanners::BaseIcannCompliant, {
+        pattern_available: /^Domain not found/
+      # pattern_disclaimer: /^Access to/,
+      # pattern_throttled: /^WHOIS LIMIT EXCEEDED/,
+      }
+    end
+  end
+end
