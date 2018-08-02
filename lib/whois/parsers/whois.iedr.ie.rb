4aba1a7952db2cb56cf9e87087fd2f0e8fe8f8e7
John Thomas <thomas07@vt.edu>
2018-08-02 12:59:09 -0400

    adding more servers

---
+++ lib/whois/parsers/whois.iedr.ie.rb
@@ -0,0 +1,7 @@
+require_relative 'whois.domainregistry.ie'
+module Whois
+  class Parsers
+    class WhoisIedrIe < WhoisDomainregistryIe
+    end
+  end
+end
--- utils/domain.rb
+++ utils/domain.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby -w
-#
+
 d = ARGV.shift
 
-`ruby ./utils/mkwhois google.#{d} status_registered`
-`ruby ./utils/mkwhois fiasodnfiaownf.#{d} status_available`
+puts `ruby ./utils/mkwhois.rb google.#{d} status_registered`
+puts `ruby ./utils/mkwhois.rb fiasodnfiaownf.#{d} status_available`
 
